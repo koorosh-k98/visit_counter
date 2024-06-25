509b2e2d4e2143c104db15b772cbde83de9772aa
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-06-25 16:56:01 +0330

    add constant sizes

---
+++ lib/constants/sizes.dart
@@ -0,0 +1,12 @@
+class Sizes {
+  static const double applicationMaxWidth = 430;
+  static const double applicationMaxHeight = applicationMaxWidth * 19.5 / 9;
+  static const double addGameInputDecorationBorderRadius = 10;
+
+  //Body
+  static const double bodyFontSize = 15;
+  static const double bodyLineHeight = 1.5;
+  static const int bodyMaxLines = 3;
+
+  const Sizes._();
+}
