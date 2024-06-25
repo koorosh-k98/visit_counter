ae275ef9a1649f2a617ac97d8b8767d0b45eddf2
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-06-25 17:26:44 +0330

    add form key provider

---
+++ lib/features/create_url/data/providers/form_key_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final formKeyProvider =
+    Provider<GlobalKey<FormState>>((ref) => GlobalKey<FormState>());
