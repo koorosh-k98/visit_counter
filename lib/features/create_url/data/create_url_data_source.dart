280c5b455a978851508d8549ba999f9148802cb0
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-06-25 20:06:41 +0330

    add create_url_data_source.dart

---
+++ lib/features/create_url/data/create_url_data_source.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:visit_counter/constants/strings.dart';
+import 'package:visit_counter/constants/values.dart';
+import 'package:visit_counter/core/data/firestore_error_mapping_mixin.dart';
+import 'package:visit_counter/core/data/firestore_error_response.dart';
+import 'package:visit_counter/core/data/result.dart';
+import 'package:visit_counter/features/create_url/data/models/visit_counter.dart';
+import 'package:visit_counter/features/create_url/data/providers/firebase_db_provider.dart';
+
+const String visitCounterCollection = 'VisitCounter';
+const String usernameField = "Username";
+const String count = "Count";
+
+final createUrlDataSourceProvider = Provider<CreateUrlDataSource>(
+  (ref) => CreateUrlDataSource(db: ref.watch(firebaseDbProvider).db),
+);
+
+class CreateUrlDataSource with FirestoreErrorMappingMixin {
+  final FirebaseFirestore _db;
+
+  CreateUrlDataSource({required FirebaseFirestore db}) : _db = db;
+
+  Future<Result<FirestoreErrorResponse, String>> generateUrl(
+    String username,
+    String label,
+    int iconsIndex,
+    int colorsIndex,
+  ) async {
+    try {
+      VisitCounter visitCounter = VisitCounter(
+        username: username,
+        label: label,
+        iconsIndex: iconsIndex,
+        colorsIndex: colorsIndex,
+        count: 0,
+      );
+
+      final snapshot = await _db
+          .collection(visitCounterCollection)
+          .where(usernameField, isEqualTo: username)
+          .get();
+      if (snapshot.docs.isEmpty) {
+        await _db
+            .collection(visitCounterCollection)
+            .add(visitCounter.toFirestore())
+            .timeout(const Duration(seconds: Values.connectionTimeout));
+      } else {
+        int previousCount = snapshot.docs.first.data()[count];
+        VisitCounter newVisitCounter =
+            visitCounter.copyWith(count: previousCount);
+        await snapshot.docs.first.reference
+            .set(newVisitCounter.toFirestore(), SetOptions(merge: true))
+            .timeout(const Duration(seconds: Values.connectionTimeout));
+      }
+      return Success(Strings.counterUrl + username);
+    } on FirebaseException catch (error, _) {
+      return mapErrorResponse(error);
+    }
+  }
+}
