5d6836cf128459e249d1cc5bdecda24549becc32
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-06-25 16:54:47 +0330

    add constant strings

---
+++ lib/constants/strings.dart
@@ -0,0 +1,13 @@
+class Strings {
+  static const String appName = "Visit Counter";
+  static const String username = "User Name";
+  static const String label = "Label";
+  static const String usernameHint = "Your Favorite User Name";
+  static const String labelHint = "Profile Views";
+  static const String enter = "Enter";
+  static const String baseUrl = "https://667043c23566a898f13c.appwrite.global/";
+  static const String counterUrl = "${baseUrl}api?username=";
+  static const String generateYourOwnLink = "Generate Your Own Link";
+
+  const Strings._();
+}
