9b3712bf872861c1d922d945959c706cfba006d3
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-06-25 16:55:01 +0330

    add constant spaces

---
+++ lib/constants/spaces.dart
@@ -0,0 +1,22 @@
+class Spaces {
+  /// 2
+  static const double xSmall = 2;
+
+  /// 4
+  static const double small = 4;
+
+  /// 8
+  static const double medium = 8;
+
+  /// 16
+  static const double large = 16;
+
+  /// 24
+  static const double xLarge = 24;
+
+  /// 32
+  static const double xxLarge = 32;
+
+  /// 48
+  static const double xxxLarge = 48;
+}
