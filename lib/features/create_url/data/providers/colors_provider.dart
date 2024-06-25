3a77fe58bfbde77f8b4c226c0b9f9950e88640bb
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-06-25 17:25:49 +0330

    add colors provider

--- lib/features/create_url/data/providers/colors_provider.dart
+++ lib/features/create_url/data/providers/colors_provider.dart
@@ -3,11 +3,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final colorsProvider = Provider<List<Color>>((ref) {
   return [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.pink,
-    Colors.amber,
+    const Color(0xFFFECACA),
+    const Color(0xFFBBF7D0),
+    const Color(0xFFBFDBFE),
+    const Color(0xFFFBCFE8),
+    const Color(0xFFFDE68A),
+  ];
+});
+
+final backgroundColorsProvider = Provider<List<Color>>((ref) {
+  return [
+    const Color(0xFFE80000),
+    const Color(0xFF12B700),
+    const Color(0xFF0015B5),
+    const Color(0xFFFF00C7),
+    const Color(0xFFD68400),
   ];
 });
 
@@ -18,6 +28,7 @@ class ColorIndex extends StateNotifier<int> {
   ColorIndex() : super(0);
 
   setIndex(int index) {
+    state = 0;
     state = index;
   }
 }
