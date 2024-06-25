e0f07e71057f37eab36fe871c096d6eda76fe1c6
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-06-25 17:25:30 +0330

    add constant colors

---
+++ lib/constants/const_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ConstColors {
+  static final addGameInputDecorationFillColor = Colors.purple.shade200;
+  static const addGameInputDecorationFocusBorderColor = Colors.greenAccent;
+  static const addGameInputDecorationErrorBorderColor = Colors.red;
+
+  //Snack Bar colors
+  static const Color snackBarIconColor = Colors.white;
+  static const Color snackBarInformationBackgroundColor = Colors.black12;
+  static const Color snackBarSuccessBackgroundColor = Colors.green;
+  static final Color snackBarWarningBackgroundColor = Colors.yellow.shade50;
+  static final Color snackBarErrorBackgroundColor = Colors.red.shade900;
+  static const Color snackBarInformationForegroundColor = Colors.black87;
+  static const Color snackBarSuccessForegroundColor = Colors.black87;
+  static const Color snackBarWarningForegroundColor = Colors.black87;
+  static const Color snackBarErrorForegroundColor = Colors.white;
+}
