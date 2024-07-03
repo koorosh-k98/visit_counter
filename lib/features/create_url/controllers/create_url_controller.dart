4cd0283b4ed2c2a41b381c935292c8eae46aee09
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-07-03 17:09:47 +0330

    update create_url_controller.dart

--- lib/features/create_url/controllers/create_url_controller.dart
+++ lib/features/create_url/controllers/create_url_controller.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:visit_counter/constants/strings.dart';
+import 'package:visit_counter/constants/values.dart';
+import 'package:visit_counter/core/providers/scroll.dart';
 import 'package:visit_counter/features/create_url/data/providers/create_url_data_provider.dart';
+import 'package:visit_counter/features/create_url/data/providers/form_key_provider.dart';
+import 'package:visit_counter/services/snack_bar_service/snack_bar_request.dart';
+import 'package:visit_counter/services/snack_bar_service/snack_bar_service.dart';
 
 final createUrlControllerProvider = Provider<CreateUrlController>(
   (ref) => CreateUrlController(ref: ref),
@@ -15,12 +23,35 @@ class CreateUrlController {
     String label,
     int iconsIndex,
     int colorsIndex,
+    ScrollController scrollController,
   ) {
     ref.read(createUrlDataProvider.notifier).generateUrl(
-          username,
-          label,
-          iconsIndex,
-          colorsIndex,
+        username, label, iconsIndex, colorsIndex, scrollController);
+  }
+
+  void generate(
+    String username,
+    String label,
+    int iconsIndex,
+    int colorsIndex,
+    ScrollController scrollController,
+  ) {
+    final formKey = ref.watch(formKeyProvider);
+    bool? result = formKey.currentState?.validate();
+    if (result ?? false) {
+      generateUrl(username, label, iconsIndex, colorsIndex, scrollController);
+    } else {
+      ref.read(scrollProvider).scrollToTop(scrollController);
+    }
+  }
+
+  onCopyButtonPressed(String url) {
+    Clipboard.setData(ClipboardData(text: url));
+    ref.read(snackBarServiceProvider).showSuccessSnackBar(
+          const SnackBarRequest(
+            content: Strings.copied,
+            duration: Values.copiedSnackBarDuration,
+          ),
         );
   }
 }
