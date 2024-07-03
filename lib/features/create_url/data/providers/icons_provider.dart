7f6a0d82998d39f6df4e1cd8a8d8c49d652a14d7
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2024-07-03 16:40:56 +0330

    invalidate previous icon after changing index

--- lib/features/create_url/data/providers/icons_provider.dart
+++ lib/features/create_url/data/providers/icons_provider.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:math';
 
-final iconsProvider = Provider.family<List<String>, Color>((ref, color) {
-  final hexColor = "#${color.value.toRadixString(16)}";
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:visit_counter/constants/const_colors.dart';
+import 'package:visit_counter/features/create_url/data/providers/create_url_data_provider.dart';
+
+final iconsProvider = Provider<List<String>>((ref) {
+  final hexColor = "#${ConstColors.iconsColor.value.toRadixString(16)}";
   return [
     '''<path d="M8.4 13.8C8.4 13.8 9.75 15.6 12 15.6C14.25 15.6 15.6 13.8 15.6 13.8M14.7 9.3H14.709M9.3 9.3H9.309M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12ZM15.15 9.3C15.15 9.54853 14.9485 9.75 14.7 9.75C14.4515 9.75 14.25 9.54853 14.25 9.3C14.25 9.05147 14.4515 8.85 14.7 8.85C14.9485 8.85 15.15 9.05147 15.15 9.3ZM9.75 9.3C9.75 9.54853 9.54853 9.75 9.3 9.75C9.05147 9.75 8.85 9.54853 8.85 9.3C8.85 9.05147 9.05147 8.85 9.3 8.85C9.54853 8.85 9.75 9.05147 9.75 9.3Z" fill="$hexColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>''',
     '''<path d="M11.2691 4.41115C11.5006 3.89177 11.6164 3.63208 11.7776 3.55211C11.9176 3.48263 12.082 3.48263 12.222 3.55211C12.3832 3.63208 12.499 3.89177 12.7305 4.41115L14.5745 8.54808C14.643 8.70162 14.6772 8.77839 14.7302 8.83718C14.777 8.8892 14.8343 8.93081 14.8982 8.95929C14.9705 8.99149 15.0541 9.00031 15.2213 9.01795L19.7256 9.49336C20.2911 9.55304 20.5738 9.58288 20.6997 9.71147C20.809 9.82316 20.8598 9.97956 20.837 10.1342C20.8108 10.3122 20.5996 10.5025 20.1772 10.8832L16.8125 13.9154C16.6877 14.0279 16.6252 14.0842 16.5857 14.1527C16.5507 14.2134 16.5288 14.2807 16.5215 14.3503C16.5132 14.429 16.5306 14.5112 16.5655 14.6757L17.5053 19.1064C17.6233 19.6627 17.6823 19.9408 17.5989 20.1002C17.5264 20.2388 17.3934 20.3354 17.2393 20.3615C17.0619 20.3915 16.8156 20.2495 16.323 19.9654L12.3995 17.7024C12.2539 17.6184 12.1811 17.5765 12.1037 17.56C12.0352 17.5455 11.9644 17.5455 11.8959 17.56C11.8185 17.5765 11.7457 17.6184 11.6001 17.7024L7.67662 19.9654C7.18404 20.2495 6.93775 20.3915 6.76034 20.3615C6.60623 20.3354 6.47319 20.2388 6.40075 20.1002C6.31736 19.9408 6.37635 19.6627 6.49434 19.1064L7.4341 14.6757C7.46898 14.5112 7.48642 14.429 7.47814 14.3503C7.47081 14.2807 7.44894 14.2134 7.41394 14.1527C7.37439 14.0842 7.31195 14.0279 7.18708 13.9154L3.82246 10.8832C3.40005 10.5025 3.18884 10.3122 3.16258 10.1342C3.13978 9.97956 3.19059 9.82316 3.29993 9.71147C3.42581 9.58288 3.70856 9.55304 4.27406 9.49336L8.77835 9.01795C8.94553 9.00031 9.02911 8.99149 9.10139 8.95929C9.16534 8.93081 9.2226 8.8892 9.26946 8.83718C9.32241 8.77839 9.35663 8.70162 9.42508 8.54808L11.2691 4.41115Z" fill="$hexColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>''',
@@ -20,14 +23,25 @@ final iconsProvider = Provider.family<List<String>, Color>((ref, color) {
   ];
 });
 
+final iconProvider = Provider<String>((ref) {
+  final icons = ref.watch(iconsProvider);
+  final index = ref.watch(iconsIndexProvider);
+  return index >= icons.length
+      ? icons[Random().nextInt(icons.length)]
+      : icons[index];
+});
+
 final iconsIndexProvider =
-    StateNotifierProvider<IconIndex, int>((ref) => IconIndex());
+    StateNotifierProvider<IconIndex, int>((ref) => IconIndex(ref));
 
 class IconIndex extends StateNotifier<int> {
-  IconIndex() : super(0);
+  Ref ref;
+
+  IconIndex(this.ref) : super(0);
 
   setIndex(int index) {
     state = 0;
     state = index;
+    ref.invalidate(createUrlDataProvider);
   }
 }
